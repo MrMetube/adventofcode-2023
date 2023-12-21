e592bc0e82fb3b3bcf16f7eb5a88474b021619e2
Viktor von Westarp <viktor.grafvonwestarp@student.hs-anhalt.de>
2023-12-22 00:55:16 +0100

    cleanup

--- src/Day19.java
+++ src/Day19.java
@@ -5,121 +5,10 @@ import java.util.List;
 
 public class Day19 implements Day {
 
-	enum Category {x, m, a, s}
-
-	enum Comparator {
-		GreaterThan, LessThan;
-
-		static Comparator of(String s) {
-			return switch (s) {
-				case ">" -> GreaterThan;
-				case "<" -> LessThan;
-				default -> throw new Error("Bad comparator sign: " + s);
-			};
-		}
-		@Override
-		public String toString() {
-			return switch (this) {
-				case GreaterThan -> ">";
-				case LessThan -> "<";
-			};
-		}
-	}
-
-	static record Rule(Category cat, Comparator comp, int value, String target) {
-		public String  toString(){ return "%s %s %d -> %s".formatted(cat, comp, value, target); }
-
-		boolean apply(int[] part) {
-			return switch (comp) {
-				case GreaterThan -> part[cat.ordinal()] > value;
-				case LessThan -> part[cat.ordinal()] < value;
-			};
-		}
-
-		List<Part> apply(Part part){
-			List<Part> res = new ArrayList<>();
-			long[] range = part.xmas[cat.ordinal()];
-			boolean splitInLowerBound = value > range[0];
-			boolean splitInUpperBound = value < range[1];
-
-			switch(comp){
-				case GreaterThan -> {
-					if(splitInUpperBound){
-						if(splitInLowerBound){ // split
-							res.add(new Part(part, cat, range[0], value, part.target)); // lower
-							res.add(new Part(part, cat, value+1, range[1], target)); // upper
-						}else{ // fully applies
-							res.add(new Part(part, target));
-						}
-					}
-				}
-				case LessThan -> {
-					if(splitInLowerBound){
-						if(splitInUpperBound){ // split
-							res.add(new Part(part, cat, range[0], value-1, target)); // lower
-							res.add(new Part(part, cat, value, range[1], part.target)); // upper
-						}else{ // fully applies
-							res.add(new Part(part, target));
-						}
-					}
-				}
-			}
-			return res;
-		}
-	}
-
-	static class Workflow extends ArrayList<Rule> {
-		final String name;
-		String defaultTarget;
-		
-		Workflow(String name){ this.name = name; }
-
-		String consider(int[] part) {
-			for (Rule r : this) if (r.apply(part)) return r.target;
-			return defaultTarget;
-		}
-
-		List<Part> consider(Part part){
-			List<Part> todo = new ArrayList<>();
-			List<Part> next = new ArrayList<>();
-			List<Part> done = new ArrayList<>();
-			todo.add(part);
-
-			for (Rule rule : this){
-				while(!todo.isEmpty()){
-					Part p = todo.removeFirst();
-					var list = rule.apply(p);
-					if(!list.isEmpty()){
-						for (Part lp : list) {
-							(lp.target.equals(name) ? next : done).add(lp);
-						}
-					}else{
-						todo.add(p);
-					}
-				}
-				todo.addAll(next);
-				next.clear();
-			}
-			todo.replaceAll(p -> p.target.equals(name) ? new Part(p, defaultTarget) : p);
-			done.addAll(todo);
-			return done;
-		}
-	}
-
-	static class Entry {
-		int[] part;
-		String target;
-
-		Entry(int[] p, String t) {
-			part = p;
-			target = t;
-		}
-	}
-
 	@Override
 	public long part1(List<String> lines) {
 		HashMap<String, Workflow> workflows = parseWorkflows(lines);
-		List<Entry> entries = new ArrayList<>();
+		List<Part> entries = new ArrayList<>();
 
 		boolean workflowsParsed = false;
 		for (String line : lines) {
@@ -135,20 +24,20 @@ public class Day19 implements Day {
 					int value = Integer.parseInt(part.substring(2));
 					values[i] = value;
 				}
-				entries.add(new Entry(values, "in"));
+				entries.add(new Part(values, "in"));
 			}
 		}
 
 		long sum = 0;
 		while (!entries.isEmpty()) {
-			Entry e = entries.getFirst();
+			Part e = entries.getFirst();
 			loop: while (true) {
 				switch (e.target) {
-					case "A": sum += e.part[0] + e.part[1] + e.part[2] + e.part[3];
+					case "A": sum += e.xmas[0] + e.xmas[1] + e.xmas[2] + e.xmas[3];
 					case "R":
 						entries.removeFirst();
 						break loop;
-					default: e.target = workflows.get(e.target).consider(e.part);
+					default: e.target = workflows.get(e.target).consider(e.xmas);
 				}
 			}
 		}
@@ -159,57 +48,84 @@ public class Day19 implements Day {
 	@Override
 	public long part2(List<String> lines) {
 		HashMap<String, Workflow> workflows = parseWorkflows(lines);
-		
-
-		List<Part> todo = new ArrayList<>();
-		todo.add(new Part());
-
-		List<Part> accepted = new ArrayList<>();
-		List<Part> rejected = new ArrayList<>();
+		List<ComplexPart> accepted = new ArrayList<>();
+		List<ComplexPart> todo = new ArrayList<>();
+		todo.add(new ComplexPart());
 		
 		while(!todo.isEmpty()){
-			Part r = todo.removeFirst();
+			ComplexPart r = todo.removeFirst();
 			switch(r.target){
-				case "A" : accepted.add(r); break;
-				case "R" : rejected.add(r); break;
+				case "A" : accepted.add(r);
+				case "R" : continue;
 				default : {
 					Workflow w = workflows.get(r.target);
-					List<Part> mapped = w.consider(r);
+					List<ComplexPart> mapped = w.consider(r);
 					todo.addAll(mapped);
 				}
 			}
 		}
 
-		long sum = accepted.stream()
-			// .peek(System.out::println)
-			.mapToLong(Part::value)
+		return accepted.stream()
+			.mapToLong(ComplexPart::value)
 			.sum();
-
-		System.out.println();
-
-		long antisum = rejected.stream()
-			// .peek(System.out::println)
-			.mapToLong(Part::value)
-			.sum();
-		assert sum + antisum == (long) Math.pow(4000, 4);
-		return sum;
 	}
 
-	static class Part{
+	static HashMap<String, Workflow> parseWorkflows(List<String> lines){
+		HashMap<String, Workflow> workflows = new HashMap<>();
+
+		for (String line : lines) {
+			if (line.isEmpty()) break;
+
+			String[] lineParts = line.replace("}", "").split("\\{");
+			String key = lineParts[0];
+			String[] rules = lineParts[1].split(",");
+			Workflow w = new Workflow(key);
+			String last = null;
+			for (String rule : rules) {
+				if (!rule.contains(":")) {
+					last = rule;
+					break;
+				}
+
+				String[] ruleparts = rule.splitWithDelimiters("[\\<\\>:]", 0);
+				Category cat = Category.valueOf(ruleparts[0]);
+				Comparator comp = Comparator.of(ruleparts[1]);
+				int value = Integer.parseInt(ruleparts[2]);
+				String targetString = ruleparts[4];
+				w.add(new Rule(cat, comp, value, targetString));
+			}
+			assert last != null : "default rule wasnt set";
+			w.defaultTarget = last;
+			workflows.put(key, w);
+		}
+		return workflows;
+	}
+
+	static class Part {
+		int[] xmas;
+		String target;
+
+		Part(int[] x, String t) {
+			xmas = x;
+			target = t;
+		}
+	}
+
+	static class ComplexPart{
 		long[][] xmas = new long[Category.values().length][2];
 		String target;
-		Part(){
+		ComplexPart(){
 			Arrays.fill(xmas, new long[]{1,4000});
 			target = "in";
 		}
 
-		Part(Part p, Category cat, long lower,  long upper, String target){
+		ComplexPart(ComplexPart p, Category cat, long lower,  long upper, String target){
 			for (Category c : Category.values())
 				xmas[c.ordinal()] = c == cat ? new long[]{lower, upper} : new long[]{p.c(c)[0], p.c(c)[1]};
 			this.target = target;
 		}
 
-		Part(Part p, String target){
+		ComplexPart(ComplexPart p, String target){
 			xmas = new long[][]{
 				new long[]{p.x()[0], p.x()[1]},
 				new long[]{p.m()[0], p.m()[1]},
@@ -242,35 +158,96 @@ public class Day19 implements Day {
 		}
 	}
 
-	static HashMap<String, Workflow> parseWorkflows(List<String> lines){
-		HashMap<String, Workflow> workflows = new HashMap<>();
+	enum Category {x, m, a, s}
 
-		for (String line : lines) {
-			if (line.isEmpty()) break;
+	enum Comparator {
+		GreaterThan, LessThan;
 
-			String[] lineParts = line.replace("}", "").split("\\{");
-			String key = lineParts[0];
-			String[] rules = lineParts[1].split(",");
-			Workflow w = new Workflow(key);
-			String last = null;
-			for (String rule : rules) {
-				if (!rule.contains(":")) {
-					last = rule;
-					break;
-				}
-
-				String[] ruleparts = rule.splitWithDelimiters("[\\<\\>:]", 0);
-				Category cat = Category.valueOf(ruleparts[0]);
-				Comparator comp = Comparator.of(ruleparts[1]);
-				int value = Integer.parseInt(ruleparts[2]);
-				String targetString = ruleparts[4];
-				w.add(new Rule(cat, comp, value, targetString));
-			}
-			assert last != null : "default rule wasnt set";
-			w.defaultTarget = last;
-			workflows.put(key, w);
+		static Comparator of(String s) {
+			return switch (s) {
+				case ">" -> GreaterThan;
+				case "<" -> LessThan;
+				default -> throw new Error("Bad comparator sign: " + s);
+			};
 		}
-		return workflows;
+		@Override
+		public String toString() {
+			return switch (this) {
+				case GreaterThan -> ">";
+				case LessThan -> "<";
+			};
+		}
+	}
+
+	static record Rule(Category cat, Comparator comp, int value, String target) {
+		public String  toString(){ return "%s %s %d -> %s".formatted(cat, comp, value, target); }
+
+		boolean apply(int[] part) {
+			return switch (comp) {
+				case GreaterThan -> part[cat.ordinal()] > value;
+				case LessThan    -> part[cat.ordinal()] < value;
+			};
+		}
+
+		List<ComplexPart> apply(ComplexPart part){
+			List<ComplexPart> res = new ArrayList<>();
+			long[] range = part.xmas[cat.ordinal()];
+			boolean splitInLowerBound = value > range[0];
+			boolean splitInUpperBound = value < range[1];
+
+			if( splitInLowerBound && !splitInUpperBound && comp == Comparator.LessThan ||
+				splitInUpperBound && !splitInLowerBound && comp == Comparator.GreaterThan)
+					res.add(new ComplexPart(part, target));
+			else if(splitInUpperBound && splitInLowerBound){
+				switch(comp){
+					case GreaterThan -> {
+						res.add(new ComplexPart(part, cat, range[0], value, part.target)); // lower
+						res.add(new ComplexPart(part, cat, value+1, range[1], target)); // upper
+					}
+					case LessThan -> {
+						res.add(new ComplexPart(part, cat, range[0], value-1, target)); // lower
+						res.add(new ComplexPart(part, cat, value, range[1], part.target)); // upper
+					}
+				}
+			}
+
+			return res;
+		}
+	}
+
+	private static class Workflow extends ArrayList<Rule> {
+		final String name;
+		String defaultTarget;
+
+		Workflow(String name){ this.name = name; }
+
+		String consider(int[] part) {
+			for (Rule r : this) if (r.apply(part)) return r.target;
+			return defaultTarget;
+		}
+
+		List<ComplexPart> consider(ComplexPart part){
+			List<ComplexPart> todo = new ArrayList<>();
+			List<ComplexPart> next = new ArrayList<>();
+			List<ComplexPart> done = new ArrayList<>();
+			todo.add(part);
+
+			for (Rule rule : this){
+				while(!todo.isEmpty()){
+					ComplexPart p = todo.getFirst();
+					var list = rule.apply(p);
+					if(!list.isEmpty()){
+						for (ComplexPart lp : list) (lp.target.equals(name) ? next : done).add(lp);
+						todo.removeFirst();
+					}
+				}
+				todo.addAll(next);
+				next.clear();
+			}
+			todo.replaceAll(p -> p.target.equals(name) ? new ComplexPart(p, defaultTarget) : p);
+			done.addAll(todo);
+			return done;
+		}
 	}
 
 }
